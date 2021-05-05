f41b3e11964604dc528562873fd077d72a1b0f9e
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-05 21:43:56 +0200

    agg script

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+
+    el: '#app',
+    data: {
+        counterPhoto: 0,
+        photos: [
+            '/img/gighen.jpg',
+            '/img/lupen.jpg',
+            '/img/ragazza.jpg',
+            '/img/zenigada.jpg',
+        ]
+
+    },
+    methods: {
+        nextPhoto() {
+            this.counterPhoto++
+                if (this.counterPhoto === this.photos.length) this.counterPhoto = 0;
+        },
+        prevPhoto() {
+            this.counterPhoto--
+                if (this.counterPhoto < 0) this.counterPhoto = this.photos.length - 1;
+        },
+
+    }
+
+
+});
+//console.log(app);
